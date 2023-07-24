37404519dcd13be4c23fe1f3da3f287c4c792cf2
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-07-24 23:31:32 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,33 @@
+"use strict" //treat all js code as never version
+
+//alert("hello")//we are using node js,not browser
+
+// code readability should be high
+
+
+let name="chhaya"
+let age=18
+let isLoggedIn=false
+console.table([name,age, isLoggedIn])
+
+// datatpypes
+
+/*
+// primitive datatype
+numer => 2 to power 53
+bigint
+string =>""
+boolean=>true/false
+null=>standalone value
+undefined=> does not inistialled value
+symbol=> find unique
+
+*/
+// typeof=> check the datatype
+
+// null is a objecti9
+
+// non primitive datatype
+/*
+object
+*/
