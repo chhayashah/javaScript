91463acb062dfbf5c37842eaacd9ecf7642c4269
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-08-06 23:40:35 +0530

    Add files via upload

---
+++ scopes.js
@@ -0,0 +1,22 @@
+// let a=10
+// const b=20
+// var c=30
+
+// console.table([a,b,c])
+
+{}//scope
+
+
+
+// var c=300// global scope
+
+
+// inside the the curly braces is knows as block scope
+if(true){
+    let a=10
+    const b=20
+    // var c=30
+}
+// console.log(a)
+// console.log(b)
+console.log(c)
