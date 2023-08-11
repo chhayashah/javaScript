c230069637f94a81408bbc527ee060e290b7b070
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-08-11 08:19:07 +0530

    Add files via upload

---
+++ switch_statement.js
@@ -0,0 +1,37 @@
+// switch (key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+
+switch(month){
+    case 1:
+        console.log('januray');
+        break;
+    case 2:
+        console.log('feb');
+        break;
+    case 3:
+        console.log('march');
+        break;
+    case 4:
+        console.log('april');
+        break;
+    case 5:
+        console.log('may');
+        break;
+    case 6:
+        console.log('june');
+        break;
+    case 7:
+        console.log('july');
+        break;
+    default:
+        console.log('invalid syntax');
+}
+
+// break control flow ko break kar deta hai
