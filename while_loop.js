59f886a824d035159fcbd26c6714b3271409eec4
chhaya shah <121175397+chhayashah@users.noreply.github.com>
2023-08-12 06:44:45 +0530

    Add files via upload

---
+++ while_loop.js
@@ -0,0 +1,13 @@
+let index=0;
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2;
+    
+}
+
+let myArray=['fish','banana','apple'];
+let arr=0
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`)
+    arr=arr+1;
+}
